57cf23745904c527a45989d672cfe846b4a4c1e9
emonhossan207 <emonhossan207@gmail.com>
2026-03-04 20:28:45 +0600

    user session add

--- script/script.js
+++ script/script.js
@@ -7,29 +7,52 @@ const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
    fetch(url)
    .then(res => res.json())
-   .then(data => console.log(data));
+   .then(data => displayLevelWord(data.data));
+   
 
 };
 
-const displayLevelWord = (word) =>{
+const displayLevelWord = (words) => {
     const wordContainer = document.getElementById("word-container");
-    // wordContainer.innerHTML = "";
+    wordContainer.innerHTML = "";
+
+    if(words.length == 0) {
+         wordContainer.innerHTML = `
+         <div class=" text-center bg-sky-100 col-span-full rounded py-10 space-y-6 font-bangla">
+         <img class="mx-auto" src="image/alert-error.png" />
+        <p class="text-[14px] font-medium text-gray-600">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="text-4xl font-bold">নেক্সট Lesson এ যান</h2>
+     </div>`;
+        return
+    };
 
     words.forEach((word) => {
-        console.log(word);
         const card = document.createElement("div");
+
         card.innerHTML = `
-        <p>cat</p>
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl ">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
+            <p class="font-semibold">Meaning / Pronunciation</p>
+            <div class="font-medium text-2xl font-bangla text-gray-500">
+            <h2 class="font-bold text-2xl ">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
+                "${word.meaning ? word.meaning :"অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি" }"
+            </div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+                    <i class="fa-solid fa-circle-info"></i>
+                </button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+                    <i class="fa-solid fa-volume-high"></i>
+                </button>
+            </div>
+        </div>
         `;
+
         wordContainer.append(card);
     });
-
-
 };
 
 const displayLesson = (lessons) => {
-
-   
     // 1. get the container & empty
     const levelContainer = document.getElementById("level-container");
     levelContainer.innerHTML = "";
